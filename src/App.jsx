e0e9dd501b3420fa2b405374bb5a98565eb412d8
TANNE123 <rajeshtanni2001@gmail.com>
2024-08-29 09:15:46 +0530

    logo-cards-button

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import CardTodo from "./components/todo-card/card"
+import ButtonEvenOdd from "./components/todo-list/button-task"
+import LogoComponent from "./logo-components"
+
+
+const App=()=>{
+  return(
+    <>
+   <CardTodo/>
+    <ButtonEvenOdd/>
+   <LogoComponent/>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/todo-list/useage.jsx
@@ -0,0 +1,17 @@
+
+
+
+// export const button=()=>{
+
+//     const rseBtn=[]
+
+//     for(let i=1;i<=10;i++){
+//         const result=<button onClick={()=>clickHandler()}>Button</button>
+//         rseBtn.push(result)
+
+//     }
+//     return rseBtn
+
+// }
+// button()
+
